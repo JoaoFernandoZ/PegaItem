e258672abb71e9bd8d09ab39036ef456ed41a355
João Fernando <joaofernandooliveirazacarias@gmail.com>
2024-05-14 08:43:12 -0300

    Mais configurações e janela adicionada

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ __TamanhoJanela = (800,500)
 __TamanhoPlayer = (40,40)
 __PosicaoInicial    = ((__TamanhoJanela[0]/2)-__TamanhoPlayer[0]/2  , __TamanhoJanela[1]-__TamanhoPlayer[1])
 
-__TamanhoCarro      = (50,50)
-__Controles = (pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d, 0)
+__TamanhoCarro  = (50,50)
+__Controles     = (pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d, 0)
 
+Jogadores       = []
+
+Tela            = pygame.display.set_mode(__TamanhoJanela)
+pygame.display.set_caption("Pega Item")
+
+EstaRodando = True
+
+while EstaRodando:
+    Eventos = pygame.event.get()
+
+    for evento in Eventos:
+        if evento.type == pygame.QUIT: EstaRodando = False
+    
+    Tela.fill((25,25,25))
